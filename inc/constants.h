9a259fee44d77a2698d537529caf87e57fa568fb
Thomas Finch <33233510+fincht96@users.noreply.github.com>
2019-01-14 14:15:09 +0000

    Create constants.h

---
+++ inc/constants.h
@@ -0,0 +1,22 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+
+// Window constants
+static const int sScreenWidth = 1280;
+static const int sScreenHeight = 720;
+
+
+// Raster pixel array constants
+static const int sRasterPixelWidth = 160;
+static const int sRasterPixelHeight = 100;
+
+
+// Arcade racer constants
+static const int sCarWidth = 7;
+
+
+
+
+#endif
